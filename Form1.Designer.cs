a063ecfd32f334202c6f7a4b9e057e4aef7bd962
Hakka Halmiev <hakkahalmiev@gmail.com>
2025-10-14 23:13:36 +0300

    First setting up  timer.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -37,20 +37,28 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(217, 128);
+            this.label1.Location = new System.Drawing.Point(374, 34);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.Size = new System.Drawing.Size(42, 16);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Nums";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(152, 170);
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(302, 67);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(164, 65);
+            this.button1.Size = new System.Drawing.Size(164, 45);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,27 @@ namespace Usage_of_Timer
         {
             InitializeComponent();
         }
+        int sayac = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            
+                sayac++;
+                label1.Text = sayac.ToString();
+
+            if (sayac >= 100)
+                {
+                    timer1.Stop();
+                    MessageBox.Show("Süre doldu");
+            }
+
+
+
+        }
     }
 }
